f3bd724a23a1db4c169aec719eb0a8c0ab7b775e
SKrishnamoorthy <34112804+Vultimate1@users.noreply.github.com>
2019-01-15 18:58:32 -0500

    Add files via upload

---
+++ src/main/java/frc/team4909/robot/subsystems/Drivetrain.java
@@ -0,0 +1,137 @@
+package frc.team4909.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+
+  private static Joystick rightStick = new Joystick(1);
+  private static Joystick leftStick = new Joystick(2);
+  private static DifferentialDrive myDrive = new DifferentialDrive(m_Left, m_Right);
+
+
+
+  /**
+
+   * This function is run when the robot is first started up and should be
+
+   * used for any initialization code.
+
+   */
+
+  @Override
+
+  public void robotInit() {
+    CANSparkMax m_Left = new CANSparkMax(RobotMap.leftMotorCANDevice, CANSparkMaxLowLevel.MotorType.kBrushless);
+    CANSparkMax m_Right = new CANSparkMax(RobotMap.rightMotorCANDevice, CANSparkMaxLowLevel.MotorType.kBrushless);
+
+  }
+
+
+  /**
+   * This function is called every robot packet, no matter the mode. Use
+   * this for items like diagnostics that you want ran during disabled,
+   * autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before
+   * LiveWindow and SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    
+/*     Spark x = new Spark(0);
+    Spark y = new Spark(1);
+    DigitalInput leftIRsensor = new DigitalInput(0);
+    DigitalInput RightIRsensor = new DigitalInput(1);
+    DigitalInput middleIRsensor = new DigitalInput(3);
+    lvalthrottle = m_left.getThrottle();
+    lvaltwist = m_left.getTwist();
+    lvalz = m_left.getZ();
+    rvalx = m_right.getX();
+    rvaly = m_right.getY();     
+    myDrive = new tankDrive(m_left, m_right);
+    
+    boolean valuel = leftIRsensor.get();
+    boolean valuem = middleIRsensor.get();
+    boolean valuer = rightIRsensor.get();
+
+    double leftspeed;
+    double rightspeed;
+
+    x.set(leftspeed);
+    y.set(rightspeed); 
+
+    if (valuer==false)
+    {
+      rightspeed=0;
+      delay(0.5);
+    }
+    else if (valuer==false)
+    {
+      leftspeed=0;
+      delay(0.5);
+    }
+    myDrive = tankDrive(leftspeed, rightspeed);
+  } */
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different autonomous modes using the dashboard. The sendable
+   * chooser code works with the Java SmartDashboard. If you prefer the
+   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+   * getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to
+   * the switch structure below with additional strings. If using the
+   * SendableChooser make sure to add them to the chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
+
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
+  }
+  /**
+   * This function is called periodically during operator control.
+   */    
+
+  @Override
+  public void teleopPeriodic() {
+    myDrive.ArcadeDrive(driveStick.getY(), driveStick.getX());
+    int throttle = driveStick.getThrottle();
+    int rotrate = driveStick.getTwist();
+    myDrive.ArcadeDrive(throttle, rotrate);
+
+  }
+
+  /**
+   * This function is called periodically during test mode.
+   */
+  @Override
+  public void testPeriodic() {
+  }
+}
