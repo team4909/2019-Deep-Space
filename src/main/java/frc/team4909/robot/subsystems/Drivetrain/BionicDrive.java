952279456d832e1aabbcf119669e1e5d7a4acd44
Jason Bishop <jasonbishop11@gmail.com>
2019-01-16 17:00:01 -0500

    Worked on adding last years code

--- src/main/java/frc/team4909/robot/subsystems/Drivetrain/BionicDrive.java
+++ src/main/java/frc/team4909/robot/subsystems/Drivetrain/BionicDrive.java
@@ -1,22 +1,38 @@
-package frc.team4909.robot.subsystems.Drivetrain;
+package frc.team4909.robot.subsystems.drivetrain;
 
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.team4909.robot.subsystems.drivetrain.commands.DriveOI;
+import frc.team4909.robot.operator.controllers.BionicF310;
+import frc.team4909.robot.operator.generic.BionicAxis;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class BionicDrive extends Subsystem {
 
-    public final CANSparkMax leftSide = new CANSparkMax(1, MotorType.kBrushed);
-    public final CANSparkMax rightSide = new CANSparkMax(2, MotorType.kBrushed);
+    public final CANSparkMax leftSpark = new CANSparkMax(1, MotorType.kBrushed);
+    public final CANSparkMax rightSpark = new CANSparkMax(2, MotorType.kBrushed);
 
-    public BionicDrive(CANSparkMax leftSide, CANSparkMax rightSRX) {
+    public final DriveOI defaultCommand;
+    
+    /**
+     * @param leftSide              Left Drivetrain SRX
+     * @param rightSide             Right Drivetrain SRX
+     * @param speedInputGamepad    Speed Input Gamepad/Joystick
+     * @param speedInputAxis       Speed Input Axis
+     * @param rotationInputGamepad Rotation Input Gamepad/Joystick
+     * @param rotationInputAxis    Rotation Input Axis
+     */
+
+    public BionicDrive(CANSparkMax leftSpark, CANSparkMax rightSpark, BionicF310 speedInputGamepad, BionicAxis speedInputAxis) {
         super();
+        
+        this.leftSpark =  leftSide;
+        this.rightSpark = rightSide;
 
-        this.leftSide = leftSpark;
-        this.rightSide = rightSpark;
 
-        this.defaultCommand = new DriveOI(this, leftSpark, rightSpark)
+        this.defaultCommand = new DriveOI(this, leftSide, rightSide,
+        speedInputGamepad, speedInputAxis);
     }
 
     protected void initDefaultCommand() {
---
+++ src/main/java/frc/team4909/robot/subsystems/drivetrain/commands/DriveOI.java
@@ -0,0 +1,103 @@
+package frc.team4909.robot.subsystems.drivetrain.commands;
+
+import frc.team4909.robot.subsystems.drivetrain.BionicDrive;
+import frc.team4909.robot.operator.controllers.BionicF310;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import frc.team4909.robot.operator.generic.BionicAxis;  ;
+
+
+public class DriveOI extends Command {
+    private final BionicDrive bionicDrive;
+    private final CANSparkMax leftSRX, rightSRX;
+
+    private final BionicF310 speedInputGamepad, rotationInputGamepad;
+    private final BionicAxis speedInputAxis, rotationInputAxis;
+    public double speedMultiplier, rotationMultiplier;
+    private double limitedSpeed, limitedRotation;
+
+    public DriveOI(BionicDrive bionicDrive, CANSparkMax leftSRX, CANSparkMax rightSRX,
+                   BionicF310 speedInputGamepad, BionicAxis speedInputAxis, double speedMultiplier,
+                   BionicF310 rotationInputGamepad, BionicAxis rotationInputAxis, double rotationMultiplier) {
+        requires(bionicDrive);
+        this.bionicDrive = bionicDrive;
+
+        this.leftSRX = leftSRX;
+        this.rightSRX = rightSRX;
+
+        this.speedInputGamepad = speedInputGamepad;
+        this.speedInputAxis = speedInputAxis;
+        this.speedMultiplier = speedMultiplier;
+
+        this.rotationInputGamepad = rotationInputGamepad;
+        this.rotationInputAxis = rotationInputAxis;
+        this.rotationMultiplier = rotationMultiplier;
+    }
+
+    @Override
+    protected void execute() {
+        // Calculate Change Limited Speed Value
+        double maxVelocity = 1,
+                speed = speedInputGamepad.getSensitiveAxis(speedInputAxis) * speedMultiplier,
+                speedDelta = speed - limitedSpeed,
+                speedDeltaLimit = bionicDrive.speedDeltaLimit;
+
+        if (Math.abs(speedDelta) > speedDeltaLimit)
+            speedDelta = Math.copySign(speedDeltaLimit, speedDelta);
+        limitedSpeed += speedDelta;
+
+        // Calculate Change Limited Rotation Value
+        double rotation = rotationInputGamepad.getSensitiveAxis(rotationInputAxis) * rotationMultiplier,
+                rotationDelta = rotation - limitedRotation,
+                rotationDeltaLimit = bionicDrive.rotationDeltaLimit;
+
+        if (Math.abs(rotationDelta) > rotationDeltaLimit)
+            rotationDelta = Math.copySign(rotationDeltaLimit, rotationDelta);
+        limitedRotation += rotationDelta;
+
+
+        // Calculate Left/Right Percentage Output Values
+        double leftMotorOutput, rightMotorOutput;
+
+        if (limitedSpeed > 0.0) {
+            if (limitedRotation > 0.0) {
+                leftMotorOutput = limitedSpeed - limitedRotation;
+                rightMotorOutput = Math.max(limitedSpeed, limitedRotation);
+            } else {
+                leftMotorOutput = Math.max(limitedSpeed, -limitedRotation);
+                rightMotorOutput = limitedSpeed + limitedRotation;
+            }
+        } else {
+            if (limitedRotation > 0.0) {
+                leftMotorOutput = -Math.max(-limitedSpeed, limitedRotation);
+                rightMotorOutput = limitedSpeed + limitedRotation;
+            } else {
+                leftMotorOutput = limitedSpeed - limitedRotation;
+                rightMotorOutput = -Math.max(-limitedSpeed, -limitedRotation);
+            }
+        }
+
+        // Limit Left/Right Percentage Output to -100% to 100%
+        leftMotorOutput = limit(leftMotorOutput);
+        rightMotorOutput = limit(rightMotorOutput);
+
+        // Set Output to Motors
+//        if(!bionicDrive.encoderOverride) {
+//            leftSRX.set(ControlMode.Velocity, maxVelocity * leftMotorOutput);
+//            rightSRX.set(ControlMode.Velocity, maxVelocity * rightMotorOutput);
+//        } else {
+            leftSRX.set(ControlMode.PercentOutput,  leftMotorOutput);
+            rightSRX.set(ControlMode.PercentOutput, rightMotorOutput);
+//        }
+    }
+
+
+    private double limit(double value) {
+        return Math.copySign(Math.abs(value) > 1.0 ? 1.0 : value, value);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+}
