a4e3585d31b8121a02da3ee6d4b81bb8da38a2eb
Ashwin Chitoor <ashketchup55@gmail.com>
2019-01-22 19:15:36 -0500

    Updates

--- src/main/java/frc/team4909/robot/sensors/PhotoElectricSensors.java
+++ src/main/java/frc/team4909/robot/sensors/PhotoElectricSensors.java
@@ -1,13 +1,15 @@
 package frc.team4909.robot.sensors;
 import frc.team4909.robot.Robot;
 import edu.wpi.first.wpilibj.DigitalInput;
-public class PhotoElectricSensors extends DigitalInput{  //not sure if extends or implements
+public class PhotoElectricSensors{  //not sure if extends or implements
     public static DigitalInput frontLeftSensor, 
-                             frontMiddleLeftSensor, 
-                             frontMiddleRightSensor,
-                             frontRightSensor;
-    double velocity = 0.7;
-                             
+                              frontMiddleLeftSensor, 
+                              frontMiddleRightSensor,
+                              frontRightSensor;
+    private double velocity = 0.7;
+    boolean frontLeftTripped = false;
+    boolean frontMiddleRightTripped = false;
+    
    
     public PhotoElectricSensors(){
         frontLeftSensor = new DigitalInput(0);
@@ -17,21 +19,38 @@ public class PhotoElectricSensors extends DigitalInput{  //not sure if extends o
     }
 
     public void onLine(){
-    boolean frontLeftOnLine = frontLeftSensor.get(); // true if sensor is not on line
-    boolean frontMiddleLeftOnLine = frontMiddleLeftSensor.get(); // false if sensor is on line
-    boolean frontMiddleRightOnLine = frontMiddleRightSensor.get();
-    boolean frontRightOnLine= frontRightSensor.get();
-
-    if(!frontMiddleLeftOnLine && frontMiddleRightOnLine){  //front middle left is on life, front middle right is not on line
-      Robot.drivetrainsub.arcadeDrive(velocity, velocity - 0.1);
+    boolean frontLeftNotOnLine = frontLeftSensor.get(); // true if sensor is not on line
+    boolean frontMiddleLeftNotOnLine = frontMiddleLeftSensor.get(); // false if sensor is on line
+    boolean frontMiddleRightNotOnLine = frontMiddleRightSensor.get();
+    boolean frontRightNotOnLine= frontRightSensor.get();
+    
+    Robot.drivetrainsub.arcadeDrive(velocity,velocity);
+    //Case 1: Coming in from right side at an angle
+    if(!frontLeftNotOnLine) {
+      frontLeftTripped = true;
     }
-    else if(frontMiddleLeftOnLine && !frontMiddleRightOnLine){  // front middle right is on line, froint middle left is not on line
-      Robot.drivetrainsub.arcadeDrive(velocity - 0.1, velocity);
-    }
-    else {
-      Robot.drivetrainsub.arcadeDrive(velocity, velocity);
-    }
-    }
-
-
+      if(frontMiddleRightNotOnLine) {
+        frontRightMiddleTripped = true;
+      }
+      if(!frontMiddleRightTripped) {
+        Robot.drivetrainsub.arcadeDrive(velocity,velocity);
+      }
+      else{
+            if (frontMiddleRightTripped && frontLeftTripped) { // front middle left is on life, front middle right is not on
+          // line
+            Robot.drivetrainsub.arcadeDrive(velocity, velocity - 0.1);
+          } else if (frontMiddleLeftNotOnLine && !frontLeftNotOnLine) { // front middle right is on line, froint middle left is
+                 // not on line
+            Robot.drivetrainsub.arcadeDrive(velocity - 0.1, velocity);
+          }
+          else {
+            Robot.drivetrainsub.arcadeDrive(velocity, velocity);
+          }
+        }
+      }
+    //Case 2: 
+    
 }
+
+
+
