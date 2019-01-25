c2a115eb09e746d9d332680e5b425ee64c3aa0c7
Jason Bishop <jasonbishop11@gmail.com>
2019-01-25 17:14:19 -0500

    Update DriveTrainSubsystem.java for 4 motor controllers

--- src/main/java/frc/team4909/robot/subsystems/drivetrain/DriveTrainSubsystem.java
+++ src/main/java/frc/team4909/robot/subsystems/drivetrain/DriveTrainSubsystem.java
@@ -4,6 +4,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.team4909.robot.operator.controllers.BionicF310;
@@ -12,21 +13,26 @@ public class DriveTrainSubsystem extends Subsystem{
     CANSparkMax frontLeftSparkMax, rearLeftSparkMax, frontRightSparkMax, rearRightSparkMax;
     DifferentialDrive bionicDrive;
     double speedMultiplier = 1.0;
+    SpeedControllerGroup m_left, m_right;
 
     public DriveTrainSubsystem(){
         frontLeftSparkMax = new CANSparkMax(1
-                            ,MotorType.kBrushed);
+                            ,MotorType.kBrushless);
 
-        rearLeftSparkMax = new CANSparkMax(1
-                            ,MotorType.kBrushed);
+        rearLeftSparkMax = new CANSparkMax(2
+                            ,MotorType.kBrushless);
 
-        frontRightSparkMax = new CANSparkMax(2
-                            ,MotorType.kBrushed);
+        SpeedControllerGroup m_left = new SpeedControllerGroup(frontLeftSparkMax, rearLeftSparkMax);                    
 
-        rearRightSparkMax = new CANSparkMax(2
-                            ,MotorType.kBrushed);
+        frontRightSparkMax = new CANSparkMax(3
+                            ,MotorType.kBrushless);
+
+        rearRightSparkMax = new CANSparkMax(4
+                            ,MotorType.kBrushless);
+
+        SpeedControllerGroup m_right = new SpeedControllerGroup(frontLeftSparkMax, rearLeftSparkMax);
         
-        bionicDrive = new DifferentialDrive(frontLeftSparkMax, frontRightSparkMax);
+        bionicDrive = new DifferentialDrive(m_left, m_right);
     }
 
     public void arcadeDrive(double x, double y){
