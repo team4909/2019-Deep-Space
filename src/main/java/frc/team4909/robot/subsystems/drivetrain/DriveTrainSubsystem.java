4fd07ecc91dd7863380d15c684dfba688c1a7a7c
Ashwin Chitoor <ashketchup55@gmail.com>
2019-01-30 16:35:04 -0500

    Fixes

--- src/main/java/frc/team4909/robot/subsystems/drivetrain/DriveTrainSubsystem.java
+++ src/main/java/frc/team4909/robot/subsystems/drivetrain/DriveTrainSubsystem.java
@@ -14,41 +14,40 @@ public class DriveTrainSubsystem extends Subsystem{
     CANSparkMax frontLeftSparkMax, rearLeftSparkMax, frontRightSparkMax, rearRightSparkMax;
     DifferentialDrive bionicDrive;
     double speedMultiplier = 1.0;
+    SpeedControllerGroup m_left, m_right;
 
-    public DriveTrainSubsystem(){             //Initialize motor controllers
+    public DriveTrainSubsystem(){
         frontLeftSparkMax = new CANSparkMax(1
-                            ,MotorType.kBrushed);
+                            ,MotorType.kBrushless);
 
-        rearLeftSparkMax = new CANSparkMax(1
-                            ,MotorType.kBrushed);
+        rearLeftSparkMax = new CANSparkMax(2
+                            ,MotorType.kBrushless);
 
-    SpeedControllerGroup m_left = new SpeedControllerGroup(frontLeftSparkMax, rearLeftSparkMax);
-
-        frontRightSparkMax = new CANSparkMax(2
-                            ,MotorType.kBrushed);
-
-        rearRightSparkMax = new CANSparkMax(2
-                            ,MotorType.kBrushed);
-                            
-    SpeedControllerGroup m_right = new SpeedControllerGroup(frontRightSparkMax, rearRightSparkMax);
+        SpeedControllerGroup m_left = new SpeedControllerGroup(frontLeftSparkMax, rearLeftSparkMax);                    
         
-        bionicDrive = new DifferentialDrive(m_left, m_right); //Creates new drive object
+        frontRightSparkMax = new CANSparkMax(3
+                            ,MotorType.kBrushless);
+
+        rearRightSparkMax = new CANSparkMax(4
+                            ,MotorType.kBrushless);
+
+        SpeedControllerGroup m_right = new SpeedControllerGroup(frontRightSparkMax, rearRightSparkMax);
+    
+        bionicDrive = new DifferentialDrive(m_left, m_right);
     }
 
-    public void arcadeDrive(double x, double y){
-        double xSpeed = x * speedMultiplier;
-        double ySpeed = y * speedMultiplier;
-        bionicDrive.arcadeDrive(xSpeed, ySpeed);
-    }
 
-    public void tankDrive(double x, double y){
-        double xSpeed = x;
-        double ySpeed = y;
-        bionicDrive.tankDrive(xSpeed, ySpeed);
+    public void tankDrive(double leftSpeed, double rightSpeed){
+        bionicDrive.tankDrive(leftSpeed, rightSpeed);
     }
 
     protected void initDefaultCommand(){
         setDefaultCommand(new Drive());
     }
-    
+
+
+
+
+
+
 }
