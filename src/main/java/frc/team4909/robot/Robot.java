5b32a8a177f8b2f0a012fec872c3de213ca8409d
Jason Bishop <jasonbishop11@gmail.com>
2019-01-30 15:55:43 -0500

    Initial Commit

--- src/main/java/frc/team4909/robot/Robot.java
+++ src/main/java/frc/team4909/robot/Robot.java
@@ -1,4 +1,4 @@
-package frc.team4909.robot;
+ package frc.team4909.robot;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Joystick;
@@ -11,14 +11,20 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.command.Scheduler;
+import frc.team4909.robot.commands.Linefollow;
 import frc.team4909.robot.operator.controllers.BionicF310;
 import frc.team4909.robot.operator.generic.BionicAxis;
-import frc.team4909.robot.sensors.PhotoElectricSensors;
+import frc.team4909.robot.operator.generic.BionicJoystick;
 import frc.team4909.robot.subsystems.drivetrain.DriveTrainSubsystem;
-import frc.team4909.robot.OI;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+
+//import frc.team4909.robot.subsystems.drivetrain.DriveTrainSubsystem;
+
+// import com.revrobotics.CANSparkMax;
+// import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 /**
  * The VM is configured to automatically run this class, and to call thex
@@ -28,71 +34,74 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
  * project.
  */
 public class Robot extends TimedRobot {
-  public static OI oi;
-  public Joystick leftDriveJoystick;
-  public Joystick rightDriveJoystick;  
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
-  
-  DigitalInput frontLeftSensor, frontMiddleSensor, frontRightSensor;
-
   public static DriveTrainSubsystem drivetrainsub;
-
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
   public static BionicF310 driverGamepad;
-  public static PhotoElectricSensors photoelectricsensors;
-  
-  // public static DifferentialDrive myDrive;   
-  // int velocity;
-  
+  private static Linefollow linefollow;
+
+  // LIDAR lidar1;
   /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
    */
   @Override
   public void robotInit() {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
-    driverGamepad = new BionicF310(0, 0, 0.6);  //Creates new drivergamepad object
-    drivetrainsub = new DriveTrainSubsystem(); //Creates new drivetrain subsytem object
-    photoelectricsensors = new PhotoElectricSensors();
+    driverGamepad = new BionicF310(0, 0, 0.6);
+    drivetrainsub = new DriveTrainSubsystem();
 
+    // CANSparkMax frontLeftSparkMax, rearLeftSparkMax, frontRightSparkMax,
+    // rearRightSparkMax;
+    // frontLeftSparkMax = new CANSparkMax(1
+    // ,MotorType.kBrushless);
+
+    // rearLeftSparkMax = new CANSparkMax(2
+    // ,MotorType.kBrushless);
+
+    // SpeedControllerGroup m_left = new SpeedControllerGroup(frontLeftSparkMax,
+    // rearLeftSparkMax);
+
+    // frontRightSparkMax = new CANSparkMax(3
+    // ,MotorType.kBrushless);
+
+    // rearRightSparkMax = new CANSparkMax(4
+    // ,MotorType.kBrushless);
+
+    // SpeedControllerGroup m_right = new SpeedControllerGroup(frontRightSparkMax,
+    // rearRightSparkMax);
 
   }
-  
-    
-    //frontLeftSensor = new DigitalInput(0);
-    //frontMiddleSensor = new DigitalInput(1);
-    //frontRightSensor = new DigitalInput(3);
 
-     
-
-  
   /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like diagnostics that you want ran during disabled, autonomous,
+   * teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
-    
+
   }
 
   /**
    * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
+   * between different autonomous modes using the dashboard. The sendable chooser
+   * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+   * remove all of the chooser code and uncomment the getString line to get the
+   * auto name from the text box below the Gyro
    *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
+   * <p>
+   * You can add additional auto modes by adding additional comparisons to the
+   * switch structure below with additional strings. If using the SendableChooser
+   * make sure to add them to the chooser code above as well.
    */
   @Override
   public void autonomousInit() {
@@ -111,27 +120,30 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
 
-    drivetrainsub.tankDrive(-Robot.driverGamepad.getThresholdAxis(BionicF310.LY), -Robot.driverGamepad.getThresholdAxis(BionicF310.RY)); //Calls tank function using left Y and right Y
-    //photoelectricsensors.lineFollow();
+    drivetrainsub.arcadeDrive(Robot.driverGamepad.getX(), Robot.driverGamepad.getY());
 
- 
-    /*myDrive.tankDrive(velocity, velocity);
-    boolean frontLeftOnLine = frontLeft.get();
-    boolean frontMiddleOnLine = frontMiddle.get();
-    boolean frontRightOnLine = frontRight.get();
-    if(!frontLeftOnLine && frontRightOnLine){
-      myDrive.tankDrive(velocity, velocity - 0.1);
-    }
-    if(frontLeftOnLine && !frontRightOnLine)
-      myDrive.tankDrive(velocity - 0.1, velocity);
-    }
-*/
+    driverGamepad.buttonPressed(BionicF310.A, new Linefollow() );
+
   }
+
+  // Lidar.write(0x04, 0x00);
+  // Lidar.read(0x01, count,byte1);
+
+  // Lidar.read(0x8f, 2, byte1);
+  // long lidarDist = byte1[0]*256 + byte1[1]; //distance of each beam in
+  // centimeters.
+  // System.out.println(lidarDist);
+  // Lidar.read(0x96, 2, byte1);
+  // long lidarDist = byte1[0]*256 + byte1[1]; //distance of each beam in
+  // centimeters.
+  // System.out.println(byte1[0] + " " + byte1[1]);
+  // System.out.println(lidar1.getDistance());
+
   @Override
   public void testPeriodic() {
 
   }
-    /**
+  /**
    * This function is called periodically during test mode.
    */
 }
--- src/main/java/frc/team4909/robot/subsystems/drivetrain/DriveTrainSubsystem.java
+++ src/main/java/frc/team4909/robot/subsystems/drivetrain/DriveTrainSubsystem.java
@@ -13,40 +13,41 @@ public class DriveTrainSubsystem extends Subsystem{
     CANSparkMax frontLeftSparkMax, rearLeftSparkMax, frontRightSparkMax, rearRightSparkMax;
     DifferentialDrive bionicDrive;
     double speedMultiplier = 1.0;
-    SpeedControllerGroup m_left, m_right;
 
-    public DriveTrainSubsystem(){
+    public DriveTrainSubsystem(){             //Initialize motor controllers
         frontLeftSparkMax = new CANSparkMax(1
-                            ,MotorType.kBrushless);
+                            ,MotorType.kBrushed);
 
-        rearLeftSparkMax = new CANSparkMax(2
-                            ,MotorType.kBrushless);
+        rearLeftSparkMax = new CANSparkMax(1
+                            ,MotorType.kBrushed);
 
-        SpeedControllerGroup m_left = new SpeedControllerGroup(frontLeftSparkMax, rearLeftSparkMax);                    
+    SpeedControllerGroup m_left = new SpeedControllerGroup(frontLeftSparkMax, rearLeftSparkMax);
+
+        frontRightSparkMax = new CANSparkMax(2
+                            ,MotorType.kBrushed);
+
+        rearRightSparkMax = new CANSparkMax(2
+                            ,MotorType.kBrushed);
+                            
+    SpeedControllerGroup m_right = new SpeedControllerGroup(frontRightSparkMax, rearRightSparkMax);
         
-        frontRightSparkMax = new CANSparkMax(3
-                            ,MotorType.kBrushless);
-
-        rearRightSparkMax = new CANSparkMax(4
-                            ,MotorType.kBrushless);
-
-        SpeedControllerGroup m_right = new SpeedControllerGroup(frontRightSparkMax, rearRightSparkMax);
-    
-        bionicDrive = new DifferentialDrive(m_left, m_right);
+        bionicDrive = new DifferentialDrive(m_left, m_right); //Creates new drive object
     }
 
+    public void arcadeDrive(double x, double y){
+        double xSpeed = x * speedMultiplier;
+        double ySpeed = y * speedMultiplier;
+        bionicDrive.arcadeDrive(xSpeed, ySpeed);
+    }
 
-    public void tankDrive(double leftSpeed, double rightSpeed){
-        bionicDrive.tankDrive(leftSpeed, rightSpeed);
+    public void tankDrive(double x, double y){
+        double xSpeed = x;
+        double ySpeed = y;
+        bionicDrive.tankDrive(xSpeed, ySpeed);
     }
 
     protected void initDefaultCommand(){
 
     }
-
-
-
-
-
-
+    
 }
