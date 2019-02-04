f72250bb3c15f1a7b436ee4932903a5c334efa81
Shashank Jarmale <shashankjarmale1@gmail.com>
2019-02-04 15:58:10 -0500

    Changes

--- src/main/java/frc/team4909/robot/LidarLitePWM.java
+++ src/main/java/frc/team4909/robot/LidarLitePWM.java
@@ -9,7 +9,7 @@ public class LidarLitePWM {
  * We've found this is a reasonably constant value for readings in the 25 cm to 600 cm range.
  * You can also use the offset to zero out the distance between the sensor and edge of the robot.
  */
-private static final int CALIBRATION_OFFSET = -18;
+private static final int CALIBRATION_OFFSET = -2;
 
 public Counter counter;
 
