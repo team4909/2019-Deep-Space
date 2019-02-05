7ba6f75cec8c4b3817e7d18fcfdf0f74f4aac927
Shash <shashankjarmale1@gmail.com>
2019-02-04 20:38:38 -0500

    Updates, LIDAR is working!

--- src/main/java/frc/team4909/robot/commands/Linefollow.java
+++ src/main/java/frc/team4909/robot/commands/Linefollow.java
@@ -1,67 +1,53 @@
-// package frc.team4909.robot.commands;
+package frc.team4909.robot.subsystems.drivetrain;
 
-// import edu.wpi.first.wpilibj.DigitalInput;
-// import edu.wpi.first.wpilibj.command.Command;
-// import edu.wpi.first.wpilibj.command.InstantCommand;
-// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-// import frc.team4909.robot.Robot;
-// import frc.team4909.robot.subsystems.drivetrain.DriveTrainSubsystem;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.team4909.robot.Robot;
+import frc.team4909.robot.RobotConstants;
 
-// public class Linefollow extends Command {
+public class Linefollow extends Command {
 
-//     public static DigitalInput LeftSensor, MiddleLeftSensor, MiddleRightSensor, RightSensor;
+    public static DigitalInput leftSensor, middleLeftSensor, middleRightSensor, rightSensor;
 
-//     private double velocity = 0.5;
+    public Linefollow() {
+        requires(Robot.drivetrainSubsystem);
+    }
 
-//     public Linefollow() {
-//         requires(Robot.drivetrainsub);
-//     }
+    protected void initialize() {
+        leftSensor = new DigitalInput(0);
+        middleLeftSensor = new DigitalInput(1);
+        middleRightSensor = new DigitalInput(2);
+        rightSensor = new DigitalInput(3);
+    }
 
-//     protected void initialize() {
-//         LeftSensor = new DigitalInput(0);
-//         MiddleLeftSensor = new DigitalInput(1);
-//         MiddleRightSensor = new DigitalInput(2);
-//         RightSensor = new DigitalInput(3);
-//     }
+    protected void execute() {
+        SmartDashboard.putBoolean("LEFT line following", !leftSensor.get());
+        SmartDashboard.putBoolean("RIGHT line following", !rightSensor.get());
 
-//     protected void execute() {
-//         SmartDashboard.putBoolean("LEFT line following", !LeftSensor.get());
-//         SmartDashboard.putBoolean("RIGHT line following", !RightSensor.get());
+        boolean left = middleLeftSensor.get(); // true = sensor is OFF line; false = sensor is ON line
+        boolean right = middleRightSensor.get(); // true = sensor is OFF line; false = sensor is ON line
 
-//         boolean left = MiddleLeftSensor.get();
-//         boolean right = MiddleRightSensor.get();
+        if (!left && right) { // back left is on line, back right is not on line
+            System.out.println(left + "  " + right + " off line to RIGHT");
+            Robot.drivetrainSubsystem.tankDrive(RobotConstants.fastVelocity, RobotConstants.slowVelocity);
+        } else if (left && !right) { // back right is on line, back left is not on line
+            System.out.println(left + "  " + right + " off line to LEFT");
+            Robot.drivetrainSubsystem.tankDrive(RobotConstants.slowVelocity, RobotConstants.fastVelocity);
+        } else { //
+            System.out.println(left + "  " + right + " ON LINE");
+            Robot.drivetrainSubsystem.tankDrive(RobotConstants.fastVelocity, RobotConstants.fastVelocity);
+        }
 
-//         // Should work no matter which side of the robot the cargo ship is on
+    }
 
-//         if (!left && right) { // back left is on line, back right is not on line
-//             System.out.println(left + "  " + right + " off line to RIGHT");
-//             Robot.drivetrainsub.tankDrive(0.5, 0.2);
-//         } else if (left && !right) { // back right is on line, back left is not on line
-//             System.out.println(left + "  " + right + " off line to LEFT");
-//             Robot.drivetrainsub.tankDrive(0.2, 0.5);
-//         } else {
-//             System.out.println(left + "  " + right + " ON LINE");
-//             Robot.drivetrainsub.tankDrive(velocity, velocity);
-//         }
+    protected boolean isFinished() {
+        return false; // return lidar.getDistance() < dist
+    }
 
-//     }
+    protected void end() {
+    }
 
-
-//     /*
-//      * isFinished - Our isFinished method always returns false meaning this command
-//      * never completes on it's own. The reason we do this is that this command will
-//      * be set as the default command for the subsystem. This means that whenever the
-//      * subsystem is not running another command, it will run this command. If any
-//      * other command is scheduled it will interrupt this command, then return to
-//      * this command when the other command completes.
-//      */
-//     protected boolean isFinished() {
-//         return false;
-//     }
-
-//     protected void end() {
-//     }
-
-//     protected void interrupted() {
-//     }
-// }
+    protected void interrupted() {
+    }
+}
