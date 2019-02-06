b18345298f044e390732189ff9481b7e294525a7
Jason Bishop <jasonbishop11@gmail.com>
2019-02-06 16:09:51 -0500

    Added Elevator Arm adn commands

--- src/main/java/frc/team4909/robot/subsystems/elevatorarm/ElevatorArmSubsytem.java
+++ src/main/java/frc/team4909/robot/subsystems/elevatorarm/ElevatorArmSubsytem.java
@@ -1,20 +1,35 @@
 package frc.team4909.robot.subsystems.elevatorarm;
 
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.InstantCommand;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class ElevatorArmSubsytem extends Subsystem{
     TalonSRX elevatorArmSRX;
-    Encoder elevatorArmEncoder;
 
-    public ElevatorArmSubsytem(){
+    public int holdingPosition;
+
+    public void ElevatorArmSubsytem(){
         //Elevator arm
         elevatorArmSRX = new TalonSRX(0);
+
+        elevatorArmSRX.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
     }
 
+    public InstantCommand setHeight(int height){
+        return new SetAngle(height, this);
+    }
+
+    public void holdCurrentPosition(){
+        holdingPosition = elevatorArmSRX.getSelectedSensorPosition();
+    }
+
+    public void elevatorArmSetSpeed(double speed){
+        elevatorArmSRX.set(ControlMode.PercentOutput, speed);
+    }
 
     @Override
     protected void initDefaultCommand() {
