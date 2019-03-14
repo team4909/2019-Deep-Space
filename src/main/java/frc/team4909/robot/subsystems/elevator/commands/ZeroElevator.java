a2b412df28f031466fac10a2acc3e2099131df66
Roshan Ravi <roshanravi10@gmail.com>
2019-03-14 02:09:58 -0400

    Cleanup #129

--- src/main/java/frc/team4909/robot/subsystems/elevator/commands/ZeroElevator.java
+++ src/main/java/frc/team4909/robot/subsystems/elevator/commands/ZeroElevator.java
@@ -3,7 +3,6 @@ package frc.team4909.robot.subsystems.elevator.commands;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.InstantCommand;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.team4909.robot.subsystems.elevator.ElevatorSubsystem;
 import frc.team4909.robot.Robot;
 
