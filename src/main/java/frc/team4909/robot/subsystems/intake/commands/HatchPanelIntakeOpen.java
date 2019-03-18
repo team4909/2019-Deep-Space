944eaa9db70d41afa40646fb4489b520756d30d2
Blake Bourque <Techplex.Engineer@gmail.com>
2019-03-18 19:38:13 -0400

    Rename IntakeSubystem default commands

--- src/main/java/frc/team4909/robot/subsystems/intake/commands/HatchPanelIntakeOpen.java
+++ src/main/java/frc/team4909/robot/subsystems/intake/commands/HatchPanelIntakeOpen.java
@@ -1,7 +1,6 @@
 package frc.team4909.robot.subsystems.intake.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-import frc.team4909.robot.subsystems.intake.commands.HatchPanelIntakeClose;
 import frc.team4909.robot.Robot;
 
 public class HatchPanelIntakeOpen extends Command {
