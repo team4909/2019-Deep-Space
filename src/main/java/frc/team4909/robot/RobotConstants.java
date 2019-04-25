9687c4bf2736252758c4ca2682eae212625d112b
jasonbishop11 <jasonbishop11@gmail.com>
2019-04-25 09:49:09 -0400

    working fixed arm setpoint

--- src/main/java/frc/team4909/robot/RobotConstants.java
+++ src/main/java/frc/team4909/robot/RobotConstants.java
@@ -79,7 +79,7 @@ public class RobotConstants {
 
   public static final int wristSetpointUpright = 3713; // makes arm upright
   public static final int wristSetpointCargoScore = wristSetpointUpright - 393 ; // 45 degrees up
-  public static final int wristSetpointHatch = wristSetpointUpright - 981;  // horizontal arm
+  public static final int wristSetpointHatch = wristSetpointUpright - 1005;  // horizontal arm -981
   public static final int wristSetpointCargoIn = wristSetpointUpright - 1222; // 45 degrees down
 
   /* Linefollow */
